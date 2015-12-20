c2859183e9dbb0bff8f5d413818dbbf4336fed93
CJ Maxwell <thecman16@gmail.com>
2015-12-19 23:50:03 -0500

    Fixed Menu Click Positions + Go Big or Go Home
    +Switched fr hardcoded pixel-menu positions to relative ones based off
    initial (maximum) screen size
    +made game start as big as players' screens

--- RAUG v1.py
+++ RAUG v1.py
@@ -3,10 +3,17 @@ import turtle
 
 def main():
     screen = turtle.Screen()
+    screen.setup(width=1., height=1.)
     screen.bgcolor('black')
-    height, width = boxInScreen(screen)
-    mainMenu(height, width)
-    screen.onclick(chooseMenuItem)
+    width, height = boxInScreen(screen)
+    menpos = mainMenu(width, height)
+
+    #print(menpos)
+
+    def getClickCoords(x,y):
+        chooseMenuItem(x,y,menpos)
+
+    screen.onclick(getClickCoords)
     screen.mainloop()
 
 def boxInScreen(screen):
@@ -47,15 +54,19 @@ def populateDeck():
 def createPlayer(name):
     pass
 
-def mainMenu(height, width):
+def mainMenu(width, height):
     t = turtle.Turtle()
     t.color('yellow')
     t.pu()
-    t.goto(-width/2,height/3)
+    t.goto(-width/2.5,height/3.25)
     t.pd()
     menu = ["New Game",'Load Game', 'Game Options', 'Save Game', 'Save & Quit']
-    t.write("Main Menu", font=("Monospace", 24, "bold"))
-    print("mainmenu @ ", t.pos())
+    menpos = {}
+    menpos["Main Menu"] = t.pos()
+    t.stamp()
+    print('Main Menu', t.pos())
+    t.write("Main Menu", font=("Monospace", 36, "bold"))
+    
     t.up()
     t.fd(8)
     t.pd()
@@ -63,29 +74,38 @@ def mainMenu(height, width):
     for menuitem in menu:
         levelsin += 1
         t.rt(90)
-        t.fd(30)
+        t.fd(55)
         t.lt(90)
-        t.fd(10+2*levelsin)
-        t.write(menuitem[0], font=("Monospace", 16, "underline"))
-        print(menuitem, " @ ", t.pos())
+        t.fd(15+2*levelsin)
+        menpos[menuitem] = t.pos()
+        t.stamp()
+        print(menuitem, t.pos())
+        
+        
+        t.write(menuitem[0], font=("Monospace", 24, "underline"))
+        
         t.pu()
-        t.fd(20)
-        t.write(menuitem[1:], font=("Monospace", 16, "normal"))
+        t.fd(30)
+        t.write(menuitem[1:], font=("Monospace", 24, "normal"))
         t.pd()
-        t.bk(10+2*levelsin)
+        t.bk(15+2*levelsin)
     t.hideturtle()
+    print(menpos)
+    return menpos
     
+
     
-def chooseMenuItem(x,y):
-    if 293.33 > y > 233.33:
+def chooseMenuItem(x,y,menpos):
+    print(x,y)
+    if menpos["Main Menu"][1] > y > menpos["New Game"][1]:
         print('New Game Selected')
-    if 263.33 > y > 203.33:
+    elif menpos["New Game"][1] > y > menpos["Load Game"][1]:
         print('Load Game Selected')
-    if 233.33 > y > 173.33:
+    elif menpos["Load Game"][1] > y > menpos["Game Options"][1]:
         print('Game Options Selected')
-    if 203.33 > y > 143.33:
+    elif menpos["Game Options"][1] > y > menpos["Save Game"][1]:
         print('Save Game Selected')
-    if 173.33 > y > 113.33:
+    elif menpos["Save Game"][1] > y > menpos["Save & Quit"][1]:
         print('Save & Quit Selected')
 #function calls
 main()
